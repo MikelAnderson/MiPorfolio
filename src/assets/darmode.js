d0009e859dc059790bb86652d83a9058a8c3f218
MikelAnderson <mikelanderson.s334891@cesurformacion.com>
2025-06-05 18:27:31 +0200

    Instalación y primer uso de GSAP en el porfolio.

--- src/assets/darmode.js
+++ src/assets/darmode.js
@@ -1,3 +1,12 @@
+document.documentElement.classList.toggle(
+  "dark",
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+);
+localStorage.theme = "light";
+localStorage.theme = "dark";
+localStorage.removeItem("theme");
+
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 const themeToggleBtn = document.getElementById('theme-toggle');
@@ -22,3 +31,24 @@ themeToggleBtn.addEventListener('click', function () {
 });
 
 
+
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+// the target can be selector text, an element, or an Array of elements
+let split = SplitText.create(".split", { type: "words, chars", });
+
+console.log(split.chars); // log the characters to the console  
+
+// now animate the characters in a staggered fashion
+gsap.from(split.words, {
+  duration: 2, 
+  opacity: 0, // animate from opacity: 0
+  ease:"sine.out", // ease out effect
+  stagger: 0.1
+  , // 0.05 seconds between each
+});
